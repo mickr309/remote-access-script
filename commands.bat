22336d4e7a1b9a4374e673c5c6fad0186b8cdab9
mickr309 <mickrosenau309@gmail.com>
2025-03-06 20:47:41 +0100

    Update commands.bat

--- commands.bat
+++ commands.bat
@@ -1,6 +1,12 @@
 @echo off
 
+set url=https://www.myinstants.com/media/sounds/ahhh-ik-krijg-een-stijve.mp3
+set localFile=%TEMP%\ahhh-ik-krijg-een-stijve.mp3
+
+powershell -Command "Invoke-WebRequest -Uri %url% -OutFile %localFile%"
+
 :loop
-start /min wmplayer "https://www.myinstants.com/media/sounds/ahhh-ik-krijg-een-stijve.mp3"
-timeout /t 5 /nobreak > nul
+start /min wmplayer "%localFile%"
+
+timeout /t 3 /nobreak > nul
 goto loop
