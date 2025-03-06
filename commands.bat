8ca97d06d7f11e500ac94de31a845a65f1f63070
mickr309 <mickrosenau309@gmail.com>
2025-03-06 20:36:12 +0100

    Update commands.bat

--- commands.bat
+++ commands.bat
@@ -1,6 +1,3 @@
 @echo off
 
-:loop
-start "" "C:\Program Files\VideoLAN\VLC\vlc.exe" "https://www.myinstants.com/media/sounds/ahhh-ik-krijg-een-stijve.mp3" --loop
-timeout /t 3 /nobreak > nul
-goto loop
+start /min "" "C:\Program Files\VideoLAN\VLC\vlc.exe" "https://www.myinstants.com/media/sounds/ahhh-ik-krijg-een-stijve.mp3" --loop --no-video
