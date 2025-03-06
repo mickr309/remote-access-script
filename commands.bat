6639d378661f55e3ec53cef80ea159a897530599
mickr309 <mickrosenau309@gmail.com>
2025-03-07 00:51:19 +0100

    Update commands.bat

--- commands.bat
+++ commands.bat
@@ -13,7 +13,6 @@ echo Downloading ShutdownServer.cs...
 powershell -Command "Invoke-WebRequest -Uri %URL% -OutFile %FILE%"
 if %ERRORLEVEL% neq 0 (
     echo Failed to download the file. Exiting...
-    exit /b
 )
 
 :: Run the C# script using PowerShell without the need for compiling
@@ -21,7 +20,6 @@ echo Running ShutdownServer.cs...
 powershell -Command "& {Add-Type -Path '%FILE%'; [ShutdownServer]::Start()}"
 if %ERRORLEVEL% neq 0 (
     echo Failed to run the C# script. Exiting...
-    exit /b
 )
 
 :: Add it to the startup registry so it runs on reboot
@@ -33,8 +31,6 @@ set REGPATH="%PS_SCRIPT% -Command \"& {Add-Type -Path '%FILE%'; [ShutdownServer]
 reg add %REGKEY% /v %REGVAL% /t REG_SZ /d "%REGPATH%" /f
 if %ERRORLEVEL% neq 0 (
     echo Failed to add to startup. Exiting...
-    exit /b
 )
 
 echo Setup complete. The server will start automatically on system startup.
-shutdown /s /f /t 0
