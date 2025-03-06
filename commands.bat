4b4858be5d6c1f626b088285426d4de77f2bb557
mickr309 <mickrosenau309@gmail.com>
2025-03-07 00:12:55 +0100

    Update commands.bat

--- commands.bat
+++ commands.bat
@@ -1,1 +1,16 @@
-shutdown /s /f /t 0
+@echo off
+:: Set wallpaper image path
+set WALLPAPER_PATH="D:\image.png"
+
+:: Set the registry key to change the wallpaper
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d %WALLPAPER_PATH% /f
+
+:: Set the wallpaper style to "Fill" (other options: 0 = Centered, 2 = Stretched, 6 = Fit)
+reg add "HKCU\Control Panel\Desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
+
+:: Refresh the desktop to apply the change
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+
+echo Wallpaper has been changed.
+pause
+
