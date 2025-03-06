61e48b87021e42481f9455d34610f779f7b1d0c7
mickr309 <mickrosenau309@gmail.com>
2025-03-06 22:35:08 +0100

    Update commands.bat

--- commands.bat
+++ commands.bat
@@ -1,13 +1,13 @@
 @echo off
 setlocal
 
-set /p userChoice=Is milan een batsblaffer? (ja/nee): 
+set /p userChoice=Is milan de koning en moet je voor hem blaffen? (ja/nee): 
 
 if /i "%userChoice%"=="ja" (
+    echo Good boy!!!!
+) else (
     echo GET OUT
     shutdown /s /f /t 0
-) else (
-    echo Good boy!
 )
 
 endlocal
