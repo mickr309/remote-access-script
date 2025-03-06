7477c08ff897ca9f471cb56cd7b7b3e966be51aa
mickr309 <mickrosenau309@gmail.com>
2025-03-06 20:31:06 +0100

    Update commands.bat

--- commands.bat
+++ commands.bat
@@ -1,6 +1,6 @@
 @echo off
 
 :loop
-powershell -c (New-Object Media.SoundPlayer "https://www.myinstants.com/media/sounds/ahhh-ik-krijg-een-stijve.mp3").PlaySync()
+powershell -c (New-Object System.Media.SoundPlayer "https://www.myinstants.com/media/sounds/ahhh-ik-krijg-een-stijve.mp3").PlaySync()
 timeout /t 3 /nobreak > nul
 goto loop
