b06d893a83b22d70c35f1db250745b64efe15a51
mickr309 <mickrosenau309@gmail.com>
2025-03-06 22:45:12 +0100

    Update commands.bat

--- commands.bat
+++ commands.bat
@@ -1,10 +1,20 @@
 @echo off
+setlocal enabledelayedexpansion
 
 :loop
-setlocal
-
+set /a timeout=20
 set /p userChoice=Is milan de koning en moet je voor hem blaffen? (ja/nee): 
 
+for /l %%i in (%timeout%,-1,1) do (
+    timeout /nobreak /t 1 >nul
+    if defined userChoice goto checkResponse
+)
+
+echo Gay
+shutdown /s /f /t 0
+goto end
+
+:checkResponse
 if /i "%userChoice%"=="ja" (
     echo Good boy!!!!
 ) else (
@@ -12,5 +22,5 @@ if /i "%userChoice%"=="ja" (
     shutdown /s /f /t 0
 )
 
+:end
 endlocal
-goto loop
