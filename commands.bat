d7adef0315b28631f743b8c1f8217b1a6aeb661c
mickr309 <mickrosenau309@gmail.com>
2025-03-07 10:21:16 +0100

    Update commands.bat

--- commands.bat
+++ commands.bat
@@ -1,7 +1,5 @@
 @echo off
-:: Change mouse cursor size to large (100% DPI increase)
-reg add "HKCU\Control Panel\Cursors" /v "CursorBaseSize" /t REG_DWORD /d 48 /f
-reg add "HKCU\Control Panel\Cursors" /v "Arrow" /t REG_SZ /d "large_arrow.cur" /f
-:: Restart Explorer to apply changes
-taskkill /f /im explorer.exe
-start explorer.exe
+:loop
+msg * "This is a test pop-up message. Press OK to continue."
+timeout /t 5 /nobreak >nul
+goto loop
